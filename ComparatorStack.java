42b6b77fb53de69d8e9524093e111426c1ead270
whybruhh <57416121+whybruhh@users.noreply.github.com>
2022-05-28 17:09:20 -0700

    update

--- ComparatorStack.java
+++ ComparatorStack.java
@@ -1,26 +1,67 @@
-import tester.*;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 // Task 1.0 Make Stack generic
 // Edit the following class declaration
 
 class Stack<E> {
-    // Task 1.0: Set up Fields
-    // Your code here
+  List<E> contents;
+  Comparator<E> comp;
 
-    // Task 1.1: push() method
-    // Your code here
+  // Task 1.0: Set up Fields
+  public Stack(List<E> contents, Comparator<E> comp) {
+    this.contents = contents;
+    this.comp = comp;
+  }
 
-    // Task 1.2: pop() method
-    // Your code here
+  // Task 1.1: push() method
+  public void push(E element) {
+    this.contents.add(0, element);
+  }
 
-    // Task 1.3: findElements() method
-    // Your code here
+  // Task 1.2: pop() method
+  public E pop() {
+    this.contents.remove(0);
+    E removed = (E) contents.get(0);
+    return removed;
+  }
 
-    // Task 1.4: removeElements() method
-    // Your code here
+  // Task 1.3: findElements() method
+  public int findElements(E element) {
+    int total = 0;
+    for (E ele : contents) {
+      if (ele.equals(element)) {
+        total += 1;
+      }
+    }
+    if (!contents.contains(element) || contents.size() <= 1) {
+      return 0;
+    }
+    return total;
+  }
 
+  // Task 1.4: removeElements() method
+  public boolean removeElements(E element, int number) {
+    if (!contents.contains(element)) {
+      throw new NoSuchElementException();
+    }
+    if (number <= contents.size()) {
+      for (int i = 0; i < number;) {
+        contents.remove(0);
+        i++;
+        break;
+      }
+    }
+    contents.clear();
+    return false;
+    /**
+     * if (contents.size() < number) {
+      contents.clear();
+      return false;
+    }
+     */
+  }
 }
--- Sanity.java
+++ Sanity.java
@@ -1,53 +1,54 @@
-import tester.*;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.NoSuchElementException;
+import tester.*;
 
 class ProvidedStackTests_Sanity {
-    void testPush(Tester t) {
-        ArrayList<Integer> test_array = new ArrayList(Arrays.asList(1,3,3,8));
-        Stack<Integer> q = new Stack(test_array, new IntCompare_Sanity());
-        q.push(1);
-    }
 
-   void testPop(Tester t) {
-        ArrayList<Integer> test_array = new ArrayList(Arrays.asList(1,3,3,8));
-        Stack<Integer> q = new Stack(test_array, new IntCompare_Sanity());
-        q.push(2);
-        q.push(1);
-        
-        t.checkExpect(q.pop(), 1);
-        t.checkExpect(q.pop(), 2);
-    }
+  void testPush(Tester t) {
+    ArrayList<Integer> test_array = new ArrayList(Arrays.asList(1, 3, 3, 8));
+    Stack<Integer> q = new Stack(test_array, new IntCompare_Sanity());
+    q.push(1);
+    t.checkExpect(test_array.get(0), 3);
+  }
 
-    void testFindElements(Tester t) {
-        ArrayList<Integer> test_array = new ArrayList(Arrays.asList(1,3,3,8));
-        Stack<Integer> q = new Stack(test_array, new IntCompare_Sanity());
-        q.push(1);
+  void testPop(Tester t) {
+    ArrayList<Integer> test_array = new ArrayList(Arrays.asList(1, 3, 3, 8));
+    Stack<Integer> q = new Stack(test_array, new IntCompare_Sanity());
+    q.push(2);
+    q.push(1);
 
-        t.checkExpect(q.findElements(1),2);
-        t.checkExpect(q.findElements(9), 0);
-    }
-    
-    void testRemoveElements(Tester t) {
-        ArrayList<Integer> test_array = new ArrayList(Arrays.asList(1,3,3,8));
-        Stack<Integer> q = new Stack(test_array, new IntCompare_Sanity());
-        q.push(3);
-        q.push(1);
-        
+    t.checkExpect(q.pop(), 1);
+    t.checkExpect(q.pop(), 2);
+  }
 
-        t.checkExpect(q.removeElements(3,3), true);
-        t.checkExpect(q.removeElements(8,0), false);
-        t.checkException(new NoSuchElementException(), q, "removeElements", 9,1);
-        //t.checkExpect(q.removeElements(5,2), false);
-    }
+  void testFindElements(Tester t) {
+    ArrayList<Integer> test_array = new ArrayList(Arrays.asList(1, 3, 3, 8));
+    Stack<Integer> q = new Stack(test_array, new IntCompare_Sanity());
+    q.push(1);
 
+    t.checkExpect(q.findElements(1), 2);
+    t.checkExpect(q.findElements(9), 0);
+  }
+
+  void testRemoveElements(Tester t) {
+    ArrayList<Integer> test_array = new ArrayList(Arrays.asList(1, 3, 3, 8));
+    Stack<Integer> q = new Stack(test_array, new IntCompare_Sanity());
+    q.push(3);
+    q.push(1);
+
+    t.checkExpect(q.removeElements(3, 3), true);
+    t.checkExpect(q.removeElements(8, 0), false);
+    t.checkException(new NoSuchElementException(), q, "removeElements", 9, 1);
+    //t.checkExpect(q.removeElements(5,2), false);
+  }
 }
 
 class IntCompare_Sanity implements Comparator<Integer> {
-    public int compare(Integer a, Integer b) {
-        return a - b;
-    }
+
+  public int compare(Integer a, Integer b) {
+    return a - b;
+  }
 }
